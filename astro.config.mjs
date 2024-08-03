cf1981ca43ecabf809443dd298866d6389d1a306
Victor Daniel Gil Becerra <victordyil97@gmail.com>
2024-08-03 13:29:43 +0200

    Downgrade pnpm version requirement to >=8
    This change adjusts the package.json file. It ensures compatibility with environments where pnpm 9 is not available, while still maintaining the necessary functionality.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://victoryil.dev',
-    base: 'portfolio'
+    site: 'https://victoryil.dev'
 });
